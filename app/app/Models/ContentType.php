09089a9a5816a8ecbd3cb7a8e7f4b52ce3ac4c99
Mike Wilson <mwilson@suitsmecard.com>
2020-11-29 19:08:40 +0000

    Install Laravel Scout and drivers

--- app/app/Models/ContentType.php
+++ app/app/Models/ContentType.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class ContentType extends Model
 {
   use HasFactory;
+  use Searchable;
 
   public function contents() {
     return $thia->hasMany('App\Models\Content');
